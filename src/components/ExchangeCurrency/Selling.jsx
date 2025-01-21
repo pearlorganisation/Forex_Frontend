ae4963917cd57b889af64922786cdaf43581cd9e
Abhishek Bahuguna <abhishek@pearlorganisation.com>
2025-01-21 16:16:24 +0530

    worked on api integratino

--- src/components/ExchangeCurrency/Selling.jsx
+++ src/components/ExchangeCurrency/Selling.jsx
@@ -42,8 +42,6 @@ const Selling = ({ data }) => {
                     ProductName: item?.ProductName?.value,
                     ForexAmount: Number(item?.ForexAmount),
                     INRAmount: Number(item?.INRAmount),
-
-
                 }
             })
         }
